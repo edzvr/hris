85f1a35d941163d38da5492a8ba17450e656e9ea
Edz <edzvronquillo@gmail.com>
2026-09-01 10:05:30 +0800

    Merge Alembic migration heads for Railway

---
+++ migrations/versions/p4e5f6a7b8c9_merge_password_reset_and_resume_heads.py
@@ -0,0 +1,18 @@
+"""Merge password-reset and resume migration heads.
+
+Revision ID: p4e5f6a7b8c9
+Revises: j7a9b2e5f1c3, o3c4d5e6f7a8
+"""
+
+revision = "p4e5f6a7b8c9"
+down_revision = ("j7a9b2e5f1c3", "o3c4d5e6f7a8")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
